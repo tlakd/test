877eee2492e89c8a522a1a2b169b0b0b6b073cac
GGG <GGG@DESKTOP-8N3GG4O>
2023-05-25 10:10:14 +0900

    주석

---
+++ ts/src/ts/Hello.java
@@ -0,0 +1,13 @@
+package ts;
+public class Hello {
+	public static void main(String[] args) {
+		// 로컬에서 작업했습니다.
+		System.out.println("Hello World");
+
+		System.out.println("변경");
+
+		System.out.println("세번째 커밋");
+
+		// 주석
+	}
+}
